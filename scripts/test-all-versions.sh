9582738da4cbf52f37d379ff3b9fe1be4c55f57a
Xinyang Chen <chenxinyang99@gmail.com>
2024-07-28 17:11:46 -0500

    use matrix

--- scripts/test-all-versions.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-CLAIRNOTE=sn ./test.py -o stable-sn -l "$(nix build ..#lilypond-2-24 --no-link --print-out-paths)/bin/lilypond"
-CLAIRNOTE=dn ./test.py -o stable-dn -l "$(nix build ..#lilypond-2-24 --no-link --print-out-paths)/bin/lilypond"
-
-CLAIRNOTE=sn ./test.py -o unstable-sn -l "$(nix build ..#lilypond-2-25 --no-link --print-out-paths)/bin/lilypond"
-CLAIRNOTE=dn ./test.py -o unstable-dn -l "$(nix build ..#lilypond-2-25 --no-link --print-out-paths)/bin/lilypond"
