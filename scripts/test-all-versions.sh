767ba66c1682bd68c54b0e6383f4b88739b0cd18
Xinyang Chen <chenxinyang99@gmail.com>
2024-07-28 16:56:56 -0500

    make nix less verbose

--- scripts/test-all-versions.sh
+++ scripts/test-all-versions.sh
@@ -2,8 +2,8 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-CLAIRNOTE=sn ./test.py -o stable-sn -l "$(nix build ..#lilypond-2-24 -v --no-link --print-out-paths)/bin/lilypond"
-CLAIRNOTE=dn ./test.py -o stable-dn -l "$(nix build ..#lilypond-2-24 -v --no-link --print-out-paths)/bin/lilypond"
+CLAIRNOTE=sn ./test.py -o stable-sn -l "$(nix build ..#lilypond-2-24 --no-link --print-out-paths)/bin/lilypond"
+CLAIRNOTE=dn ./test.py -o stable-dn -l "$(nix build ..#lilypond-2-24 --no-link --print-out-paths)/bin/lilypond"
 
-CLAIRNOTE=sn ./test.py -o unstable-sn -l "$(nix build ..#lilypond-2-25 -v --no-link --print-out-paths)/bin/lilypond"
-CLAIRNOTE=dn ./test.py -o unstable-dn -l "$(nix build ..#lilypond-2-25 -v --no-link --print-out-paths)/bin/lilypond"
+CLAIRNOTE=sn ./test.py -o unstable-sn -l "$(nix build ..#lilypond-2-25 --no-link --print-out-paths)/bin/lilypond"
+CLAIRNOTE=dn ./test.py -o unstable-dn -l "$(nix build ..#lilypond-2-25 --no-link --print-out-paths)/bin/lilypond"
